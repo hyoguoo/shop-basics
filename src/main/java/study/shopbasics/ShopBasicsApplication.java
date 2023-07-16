df4b47d99814d69da03299ea4519203d536b7d51
hyoguoo <hyoguoo@gmail.com>
2023-07-16 19:05:51 +0900

    init: init project

---
+++ src/main/java/study/shopbasics/ShopBasicsApplication.java
@@ -0,0 +1,12 @@
+package study.shopbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopBasicsApplication.class, args);
+    }
+}
---
+++ src/test/java/study/shopbasics/ShopBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package study.shopbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
