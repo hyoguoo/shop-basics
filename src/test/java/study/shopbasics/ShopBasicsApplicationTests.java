62f9a8140386bcdcd0084617945552897fea3c40
hyoguoo <hyoguoo@gmail.com>
2023-07-29 22:44:09 +0900

    add: MySQL support and connection test

--- src/test/java/study/shopbasics/ShopBasicsApplicationTests.java
+++ src/test/java/study/shopbasics/ShopBasicsApplicationTests.java
@@ -1,13 +1,26 @@
 package study.shopbasics;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class ShopBasicsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
+    @Test
+    @DisplayName("MySQL Connection Test")
+    void testMySQLConnection() {
+        try {
+            Integer test = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+            Assertions.assertEquals(test, 1);
+        } catch (Exception e) {
+            Assertions.fail(e.getMessage());
+        }
+    }
 }
