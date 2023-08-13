7365c307212a26cbf6ba01596c9d0c1ecda852b6
hyoguoo <hyoguoo@gmail.com>
2023-08-14 03:09:42 +0900

    add: add JPAQueryFactory bean to application

--- src/main/java/study/shopbasics/ShopBasicsApplication.java
+++ src/main/java/study/shopbasics/ShopBasicsApplication.java
@@ -1,12 +1,20 @@
 package study.shopbasics;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ShopBasicsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShopBasicsApplication.class, args);
+    }
+
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
     }
 }
