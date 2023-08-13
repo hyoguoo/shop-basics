c571d62af71437f8eac22020c6d9bb7211c0e418
hyoguoo <hyoguoo@gmail.com>
2023-08-14 04:54:05 +0900

    mod: Remove cart entity and repository

--- src/main/java/study/shopbasics/repository/CartRepository.java
+++
@@ -1,7 +0,0 @@
-package study.shopbasics.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import study.shopbasics.entity.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-}
