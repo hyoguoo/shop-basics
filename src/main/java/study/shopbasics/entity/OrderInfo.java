bfde1e453ed18bc9e83a0a9117b54e288f589840
hyoguoo <hyoguoo@gmail.com>
2023-08-26 03:45:05 +0900

    add: implement unique order number generation

--- src/main/java/study/shopbasics/entity/OrderInfo.java
+++ src/main/java/study/shopbasics/entity/OrderInfo.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Getter
@@ -24,11 +25,15 @@ public class OrderInfo {
     private List<OrderProduct> orderProducts = new ArrayList<>();
 
     @Column(name = "order_number", nullable = false)
-    private String orderNumber;
+    private String orderNumber = getUniqueOrderNumber();
 
     @Column(name = "total_amount", nullable = false)
     private BigDecimal totalAmount;
 
     @Column(name = "order_date", nullable = false)
     private LocalDateTime orderDate;
+
+    private static String getUniqueOrderNumber() {
+        return UUID.randomUUID().toString();
+    }
 }
