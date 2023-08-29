b080232e719a9e96835c6de33097b3295aff2301
hyoguoo <hyoguoo@gmail.com>
2023-08-29 17:06:22 +0900

    add: add unique constraints in UserLike entity

--- src/main/java/study/shopbasics/entity/UserLike.java
+++ src/main/java/study/shopbasics/entity/UserLike.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "user_like")
+@Table(name = "user_like", uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "product_id"}))
 public class UserLike {
 
     @Id
