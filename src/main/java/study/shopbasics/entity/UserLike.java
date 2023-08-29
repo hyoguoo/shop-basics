4ec9564564707b982a35181732ffc959d1eaf875
hyoguoo <hyoguoo@gmail.com>
2023-08-29 16:08:58 +0900

    add: add builder annotations to UserLike entity

--- src/main/java/study/shopbasics/entity/UserLike.java
+++ src/main/java/study/shopbasics/entity/UserLike.java
@@ -1,10 +1,16 @@
 package study.shopbasics.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "user_like")
 public class UserLike {
 
